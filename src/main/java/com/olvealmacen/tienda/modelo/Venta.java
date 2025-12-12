6c79cc2b7ed6d7d9d1f5e92d79f94f48ef83483a
SarabiaJ <jsarabiahoyos76@correo.unicordoba.edu.co>
2025-12-12 05:17:29 +0000

    edicion de clase base

--- src/main/java/com/olvealmacen/tienda/modelo/Venta.java
+++ src/main/java/com/olvealmacen/tienda/modelo/Venta.java
@@ -1,54 +1,34 @@
 package com.olvealmacen.tienda.modelo;
 
 public class Venta {
-    private int id;
+    private int idVenta;
     private String fecha;
-    private int idCliente;
-    private String nombreCliente;
-    private int idProducto;
-    private String nombreProducto;
-    private int cantidad;
-    private String metodoPago;
     private double total;
+    private String metodoPago;
+    private int idCliente;
 
     public Venta() { }
 
-    public Venta(int id, String fecha, int idCliente, String nombreCliente, int idProducto, String nombreProducto, int cantidad, String metodoPago, double total) {
-        this.id = id;
+    public Venta(int idVenta, String fecha, double total, String metodoPago, int idCliente) {
+        this.idVenta = idVenta;
         this.fecha = fecha;
-        this.idCliente = idCliente;
-        this.nombreCliente = nombreCliente;
-        this.idProducto = idProducto;
-        this.nombreProducto = nombreProducto;
-        this.cantidad = cantidad;
-        this.metodoPago = metodoPago;
         this.total = total;
+        this.metodoPago = metodoPago;
+        this.idCliente = idCliente;
     }
 
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public int getIdVenta() { return idVenta; }
+    public void setIdVenta(int idVenta) { this.idVenta = idVenta; }
 
     public String getFecha() { return fecha; }
     public void setFecha(String fecha) { this.fecha = fecha; }
 
-    public int getIdCliente() { return idCliente; }
-    public void setIdCliente(int idCliente) { this.idCliente = idCliente; }
-
-    public String getNombreCliente() { return nombreCliente; }
-    public void setNombreCliente(String nombreCliente) { this.nombreCliente = nombreCliente; }
-
-    public int getIdProducto() { return idProducto; }
-    public void setIdProducto(int idProducto) { this.idProducto = idProducto; }
-
-    public String getNombreProducto() { return nombreProducto; }
-    public void setNombreProducto(String nombreProducto) { this.nombreProducto = nombreProducto; }
-
-    public int getCantidad() { return cantidad; }
-    public void setCantidad(int cantidad) { this.cantidad = cantidad; }
+    public double getTotal() { return total; }
+    public void setTotal(double total) { this.total = total; }
 
     public String getMetodoPago() { return metodoPago; }
     public void setMetodoPago(String metodoPago) { this.metodoPago = metodoPago; }
 
-    public double getTotal() { return total; }
-    public void setTotal(double total) { this.total = total; }
+    public int getIdCliente() { return idCliente; }
+    public void setIdCliente(int idCliente) { this.idCliente = idCliente; }
 }
